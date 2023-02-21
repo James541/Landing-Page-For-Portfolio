3bb456270279bc35dd71f68e0e03da1681b721fa
James541 <96335020+James541@users.noreply.github.com>
2023-02-21 00:30:33 -0800

    button functionality
    added and debugged button functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+function gitHub(){
+    window.open("https://github.com/James541");
+};
+
+function linkedIn(){
+    window.open("https://www.linkedin.com/in/jamestwagner/");
+};
+
+function twitterPage(){
+    window.open("https://twitter.com/James_Wagner_");
+};
+
